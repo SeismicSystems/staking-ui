bbaa53de93ac56e7e478a8db465452e0c57b6a1c
Ameya Deshmukh <ad@seismic.systems>
2025-12-05 02:04:09 +0530

    DRY out deposit contract address by importing from seismic-viem

--- packages/indexer/ponder.config.ts
+++ packages/indexer/ponder.config.ts
@@ -3,17 +3,13 @@ import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import { depositContractAbi } from "./abis/DepositContractAbi";
-import { Address } from "viem";
+import { DEPOSIT_CONTRACT_ADDRESS } from "seismic-viem";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 dotenv.config({ path: join(__dirname, "../../.env") });
 
-const DEPOSIT_CONTRACT_ADDRESS: Address =
-  "0x00000000219ab540356cBB839Cbe05303d7705Fa";
-
-console.log("rpc url", process.env.VITE_RPC_URL);
 
 export default createConfig({
   chains: {
